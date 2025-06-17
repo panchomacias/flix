eb27c725703a2bdf0322f2f5889031a2876d6501
panchomacias <panchojmacias@gmail.com>
2025-06-17 07:25:04 -0300

    auth 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def require_signin
+    unless current_user
+      redirect_to signin_path, alert: "You must be signed in to access this section"
+    end
+  end
 end
